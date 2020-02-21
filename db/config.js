50d6c31ddd558d3394b099e043912a2eb66cd189
Priyanka Jayaswal <prjayasw@microsoft.com>
2020-02-21 14:56:44 +0530

    Changes that work

--- db/config.js
+++ db/config.js
@@ -3,23 +3,31 @@ const os = require('os');
 
 module.exports = {
   development: {
-    database: 'slack-dev',
+    uri:'postgres://postgres:postgres@127.0.0.1:5432/slack_dev',
+    database: 'slack_dev',
     host: '127.0.0.1',
     dialect: 'postgres',
     operatorsAliases: false,
+    username:"postgres",
+    password:"postgres"
   },
   test: {
-    database: 'slack-test',
+    uri:'postgres://postgres:postgres@127.0.0.1:5432/slack_test',
+    database: 'slack_test',
     host: '127.0.0.1',
     dialect: 'postgres',
     operatorsAliases: false,
-    username: process.env.DB_USERNAME_TEST || os.userInfo().username,
+    username:"postgres",
+    password:"postgres"
+    // username: process.env.DB_USERNAME_TEST || os.userInfo().username,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    disable_sql_logging: true,
+    use_env_variable: 'postgres://postgres:postgres@127.0.0.1:5432/slack_dev' || 'DATABASE_URL',
+    disable_sql_logging: false,
     operatorsAliases: false,
     ssl: true,
+    username:"postgres",
+    password:"postgres",
     dialectOptions: {
       ssl: true,
     },
